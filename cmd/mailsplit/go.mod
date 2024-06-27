6770578f8d21e6eea6f386a57525c2dbbced1cfa
Drew Walton <drewwalton19216801@gmail.com>
2024-06-27 02:17:16 -0600

    Apparently I never actually committed the mailsplit app!

---
+++ cmd/mailsplit/go.mod
@@ -0,0 +1,5 @@
+module github.com/drewwalton19216801/mailsplit/cmd/mailsplit
+
+go 1.22.4
+
+require github.com/drewwalton19216801/mailsplit v0.1.0
---
+++ cmd/mailsplit/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/drewwalton19216801/mailsplit"
+)
+
+// mailsplit is a command line tool that processes an email and saves attachments to disk
+// Usage: mailsplit <path_to_email_file>
+
+func main() {
+	// Read email from a file
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	emailContent, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create the output directory if it doesn't exist
+	outputDir := "attachments"
+	if _, err := os.Stat(outputDir); os.IsNotExist(err) {
+		err = os.Mkdir(outputDir, 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// Process the email to save attachments and get the modified email content
+	newEmailContent, err := mailsplit.ProcessEmail(string(emailContent), outputDir)
+	if err != nil {
+		log.Fatalf("Failed to process email: %v", err)
+	}
+
+	// Save the modified email content to a new file
+	outputFile, err := os.Create("output.eml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer outputFile.Close()
+	_, err = outputFile.WriteString(newEmailContent)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Email processed successfully!")
+}
